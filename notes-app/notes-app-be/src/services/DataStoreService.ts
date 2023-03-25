bc4ff9ce5ddc4dc5926aeff26795462a5fea4b47
Drex <davidokunoye003@gmail.com>
2023-03-25 08:37:15 +0100

    completed the Indexing Service

--- notes-app/notes-app-be/src/services/DataStoreService.ts
+++ notes-app/notes-app-be/src/services/DataStoreService.ts
@@ -1,52 +1,51 @@
-import {Notes} from "../entities";
+import { Notes } from "../entities";
 // @ts-ignore
-import * as db from "../datastore/notes.store.json"
-import IndexServiceImpl, {IndexService} from "./IndexService";
-
+import * as db from "../datastore/notes.store.json";
+import IndexServiceImpl, { IndexService } from "./IndexService";
 
 /**
  * @classdesc Data store interface.
  */
 export interface DataStore {
-    create(note: Notes): Promise<Notes>;
-    getNote(id: string): Notes;
-    getNotes(): Notes[];
-    update(id: string, note: Notes): Promise<boolean>;
+  create(note: Notes): Promise<Notes>;
+  getNote(id: string): Notes;
+  getNotes(): Notes[];
+  update(id: string, note: Notes): Promise<boolean>;
 
-    delete(id: string): boolean;
+  delete(id: string): boolean;
 }
 
 /**
  * @classdesc Data store implementation
  */
 class DataStoreImpl implements DataStore {
-    constructor(private readonly indexService: IndexService) {}
-    create = async (note: Notes): Promise<Notes> => {
-        db[note.id] = note;
+  constructor(private readonly indexService: IndexService) {}
+  create = async (note: Notes): Promise<Notes> => {
+    db[note.id] = note;
 
-        this.indexService.index(note);
+    this.indexService.index(note);
 
-        return note;
-    }
+    return note;
+  };
 
-    delete(id: string): boolean {
-        return false;
-    }
+  delete(id: string): boolean {
+    return false;
+  }
 
-    getNote(id: string): Notes {
-        return undefined;
-    }
+  getNote(id: string): Notes {
+    const note = db[id];
+    return note;
+  }
 
-    getNotes(): Notes[] {
-        return [];
-    }
+  getNotes(): Notes[] {
+    return [];
+  }
 
-    update = async (id: string, currentNote: Notes): Promise<boolean> => {
-        const previousNote = db[id];
-        this.indexService.reIndex(previousNote, currentNote);
-        return false;
-    }
-
+  update = async (id: string, currentNote: Notes): Promise<boolean> => {
+    const previousNote = db[id];
+    this.indexService.reIndex(previousNote, currentNote);
+    return false;
+  };
 }
 
 export default new DataStoreImpl(IndexServiceImpl);
